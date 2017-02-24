52af31256303ef1893597e3e62b52ef097c8018a
calliebradley <callie.m.bradley@gmail.com>
2017-02-23 16:47:31 -0800

    Major refactoring and beginning of new function
    Does not yet compile!

--- README.md
+++ README.md
@@ -1,21 +1,29 @@
 # codeReviewFun
+This code came originally from Katie (Insight, @kcamrine) as a sample for time-boxed code review.
 
-## Directions
-* Fork repository
-* clone to your desktop
-* make changes all in one file
-* merge to your fork
-* discuss with your group! 
+I've begun to refactor the code and add an additional function.
 
-I have a grid and a class Vehicle, which will have starting point(X, Y on the grid) and direction(one of N,E,S,W) taken from user and there will be commands, L & R turns the vehicle 90 degrees around left and right respectively and M moves the vehicle one unit to faced direction.
+## Install
+Runs on Python 2.7. Compiled in PyCharm.
 
-For instance, if the command is M and the current direction is E, the vehicle moves from (x,y) to (x+1,y) and preserve its direction.
-If the command is R, and the current direction is E, vehicle preserves its position but direction changes to S.
+## Structure
 
-My input consists of 5 lines:
+I have a grid and a class Vehicle, which will have starting point(X, Y on the grid) and direction(one of N,E,S,W)
+taken from user and there will be commands, L & R turns the vehicle 90 degrees around left and right respectively and
+M moves the vehicle one unit to faced direction.
+
+For instance, if the command is M and the current direction is E, the vehicle moves from (x,y) to (x+1,y) and preserve
+its direction. If the command is R, and the current direction is E, vehicle preserves its position but direction changes
+to S.
+
+### Inputs
+Input consists of 5 lines:
 
 The first line defines the limits of the grid; X and Y separated by space
-The second line defines the current position and facing direction for the first vehicle; X, Y and Dir are separated by space
+The second line defines the current position and facing direction for the first vehicle; X, Y and Dir are separated
+by spaces
 The third line defines the commands for the first vehicle, which is a line of string
 The fourth and fifth lines are the same as second and third except they are for the second vehicle
-Note: Vehicles are sent sequentially. If the second vehicle attempts to move to the occupied spot of the first vehicle, the command will be skipped. If any move command makes any of the vehicles move out of the grid, that command will be skipped as well. Inputs are always in expected style, so there is no need to validate inputs.
+Note: Vehicles are sent sequentially. If the second vehicle attempts to move to the occupied spot of the first vehicle,
+the command will be skipped. If any move command makes any of the vehicles move out of the grid, that command will be
+skipped as well. Inputs are always in expected style, so there is no need to validate inputs.
--- move_car.py
+++ move_car.py
@@ -1,66 +1,95 @@
-# Add overall comment describing the code
-# State Python version used, author coordinates
-# Write unit tests and try/except error handling
-# User inputs should be defined/prompted for with description
-# User inputs should be grouped together in the code
-# Typo in vehicle_two below
-# Write code in methods, avoid global variables. Don't reference global variables inside the class
-# dir could be a protected variable (for directory), should avoid
+# Refactored from kcamrine at Insight
+# Callie Bradley, callie.m.bradley@gmail.com
+# February 2017
+# Run with Python 2.7, compiled with PyCharm
 
-directions = ['N','E','S','W']
-movement = {'N': (0,1), 'E': (1,0), 'S': (0,-1), 'W':(-1,0)}
-commands = {'L': 'turn_left', 'R': 'turn_right', 'M': 'move'}
+# This code takes input to describe vehicle location and movement.
+# It outputs the new location
 
-GRID_MAX_X, GRID_MAX_Y = map(int, raw_input().split())
-
-# These are not used effectively
-first_vehicle_x = None
-first_vehicle_y = None
+# Still to add:
+## Unit tests, try/except better error handling
+## User inputs should be grouped together in the code
+## Typo in vehicle_two below
+## Write code in methods, avoid global variables. Don't reference global variables inside the class
+## dir could be a protected variable (for directory), should avoid
 
 class Vehicle():
-    def __init__(self, x, y, face):
+    """ Class to describe the vehicle coordinates """
+    def __init__(self, x, y, face, maxx, maxy):
         self.x = x
         self.y = y
-        self.dir = face
+        self.face = face
+        self.max_x = maxx
+        self.max_y = maxy
+        self.directions = ['N','E','S','W']
+        self.movement = {'N': (0,1), 'E': (1,0), 'S': (0,-1), 'W':(-1,0)}
+        self.commands = {'L': 'turn_left', 'R': 'turn_right', 'M': 'move', 'B': 'back'}
 
-    # Seems to me like movements would make more sense as a separate class
+
+class Movement():
+    """ Class to describe vehicle movement """
+    def input(self):
+        print "Welcome to vehicle descriptorland!"
+        print "You can input two vehicles' location and movement in the following way:"
+        print "    xmax ymax"
+        print "    x1 y1 face1"
+        print "    movement1"
+        print "    x2 y2 face2"
+        print "    movement2"
+        print ""
+        print "Where:"
+        print "    - 1 refers to vehicle 1 and 2 to vehicle 2"
+        print "    - x and y inputs are in integers"
+        print "    - face inputs are cardinal directions: N, S, E or W"
+        print "    - movements are L, R, F, or B for a left turn, right turn, forward or back move. Ex: LFRB"
+
     def turn_left(self):
-        self.dir = directions[(directions.index(self.dir)-1)%len(directions)]
+        self.face = directions[(directions.index(self.face)-1)%len(directions)]
 
     def turn_right(self):
-        self.dir = directions[(directions.index(self.dir)+1)%len(directions)]
+        self.face = directions[(directions.index(self.face)+1)%len(directions)]
 
-    def move(self):
-        new_x = self.x + movement[self.dir][0]
-        new_y = self.y + movement[self.dir][1]
+    def forward(self):
+        new_x = self.x + movement[self.face][0]
+        new_y = self.y + movement[self.face][1]
 
-        # This if condition doesn't do anything ?
-        if new_x != first_vehicle_x or new_y != first_vehicle_y:
-            if new_x in xrange(GRID_MAX_X+1):
-                self.x = new_x
-            if new_y in xrange(GRID_MAX_Y+1):
-                self.y = new_y
+    def back(self):
+        new_x = self.x + movement[self.face][-1]
+        new_y = self.y + movement[self.face][0]
 
-vehicle_one_pos = raw_input().split()
-vehicle_one_commands = raw_input()
+    def move(self,vehicle,movemt):
+        """ This method does the main work of finding movement and returning new position """
+        # 1. Check if vehicles overlap
+        if abs(vehicle.x) > vehicle.max_x:
+            vehicle.x = vehicle.max_x
+        if abs(vehicle.y) > vehicle.max_y:
+            vehicle.y = vehicle.max_y
 
-vehicle_one = Vehicle(int(vehicle_one_pos[0]), int(vehicle_one_pos[1]), vehicle_one_pos[2])
-for command in vehicle_one_commands:
-    # better to write as method call rather than eval - eval is slower and more command-line centric
-    eval("vehicle_one.{0}()".format(commands[command]))
-
-# These definitions do not persist so are not actually used
-first_vehicle_x = vehicle_one.x
-first_vehicle_y = vehicle_one.y
+        # 2. Assign movement based on command
+        # 3. Return new position
 
 
-vehicle_two_pos = raw_input().split()
-vehicle_two_commands = raw_input()
+if __name__ == '__main__':
 
-# Typo: should be vehicle_two_pos
-vehicle_two = Vehicle(int(vehicle_two_pos[0]), int(vehicle_two_pos[1]), vehicle_two_pos[2])
-for command in vehicle_two_commands:
-    eval("vehicle_two.{0}()".format(commands[command]))
+    # Take input
+    Movement.input()
+    max_x, max_y = map(int, raw_input().split())
+    vehicle_one_pos = raw_input().split()
+    vehicle_one_commands = raw_input()
+    vehicle_two_pos = raw_input().split()
+    vehicle_two_commands = raw_input()
 
-print vehicle_one.x, vehicle_one.y, vehicle_one.dir
-print vehicle_two.x, vehicle_two.y, vehicle_two.dir
+    # Assign positions
+    vehicle_one = Vehicle(int(vehicle_one_pos[0]), int(vehicle_one_pos[1]), vehicle_one_pos[2], max_x, max_y)
+    vehicle_two = Vehicle(int(vehicle_two_pos[0]), int(vehicle_two_pos[1]), vehicle_two_pos[2], max_x, max_y)
+
+    # Move vehicles
+    for command in vehicle_one_commands:
+        vehicle_one = Movement.move(vehicle_one,command)
+
+    for command in vehicle_two_commands:
+        vehicle_two = Movement.move(vehicle_two,command)
+
+    # Return output
+    print "Vehicle one's new position: (", vehicle_one.x,",", vehicle_one.y,"), facing ", vehicle_one.face
+    print "Vehicle two's new position: (", vehicle_two.x,",", vehicle_two.y,"), facing ", vehicle_two.face
